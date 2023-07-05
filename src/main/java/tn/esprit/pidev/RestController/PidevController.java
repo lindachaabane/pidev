d62c9c7cb39f653939b3326f4bcc62495396b2ba
Linda Chaabane <linda.chaabane@esprit.tn>
2023-07-05 21:39:29 +0200

    Gestion des departements

--- src/main/java/tn/esprit/pidev/RestController/PidevController.java
+++ src/main/java/tn/esprit/pidev/RestController/PidevController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import tn.esprit.pidev.entities.Departement;
 import tn.esprit.pidev.entities.Role;
 import tn.esprit.pidev.entities.User;
 import tn.esprit.pidev.security.services.UserDetailsServiceImpl;
@@ -58,6 +59,78 @@ public class PidevController {
             return ResponseEntity.notFound().build();
         }
     }
+    /** GESTION DES DEPARTEMENTS **/
 
+    @PostMapping("/add_dep")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Departement> createDepartement(@RequestParam("name") String name) {
+        Departement createdDepartement = userService.createDepartement(name);
+        return ResponseEntity.ok(createdDepartement);
+    }
+
+    @GetMapping("/all_dep")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<List<Departement>> getAllDepartments() {
+        List<Departement> departments = userService.getAllDepartments();
+        return ResponseEntity.ok(departments);
+    }
+
+    @GetMapping("/users_no_dep")
+    @PreAuthorize("hasRole('ADMIN')")
+    List<User> getUsersWithoutDepartment(){
+        return userService.getUsersWithoutDepartment();
+    }
+
+   /* @PostMapping("/affect/{userId}/assign-department/{departmentId}")
+    public ResponseEntity<String> assignUserToDepartment(@PathVariable("userId") Long userId, @PathVariable("departmentId") Long departmentId) {
+        userService.assignUserToDepartment(userId, departmentId);
+        return ResponseEntity.ok("Utilisateur affecté au département avec succès.");
+    }*/
+
+    @PostMapping("/affect")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> assignUserToDepartment(@RequestParam("user_id")Long userId , @RequestParam("dep_id") Long departmentId) {
+        System.out.println(userId);
+        System.out.println("******************");
+        System.out.println(departmentId);
+        userService.assignUserToDepartment(userId, departmentId);
+
+        return ResponseEntity.ok().build(); // Renvoie une réponse avec le statut 200 (OK)
+    }
+
+    @GetMapping("/department/{departmentId}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<List<User>> getUsersByDepartment(@PathVariable Long departmentId) {
+        List<User> users = userService.getUsersByDepartment(departmentId);
+        for(User u : users){
+            System.out.println(u.getUsername());
+        }
+        return ResponseEntity.ok(users);
+    }
+    /*@GetMapping("/getdepname/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String getDepName(@PathVariable Long id){
+        return userService.getDepName(id);
+    }*/
+
+    @PutMapping("/{userId}/remove-department")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Void> removeDepartmentFromUser(@PathVariable Long userId, @RequestBody(required = false) Map<String, Object> requestBody) {
+        userService.removeDepartmentFromUser(userId);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/delete_dep/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<String> deleteDepartement(@PathVariable("id") Long departementId) {
+        try {
+            userService.deleteDepartement(departementId);
+            return ResponseEntity.ok().body("Departement deleted successfully.");
+        } catch (NoSuchElementException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Departement not found", e);
+        } catch (IllegalStateException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        }
+    }
 
 }
